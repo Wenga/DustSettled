3bfce1f7c544dac5541567e5b250ae6d48059764
Wenga <mankayanwenga@gmail.com>
2024-09-28 22:08:04 -0700

    added sound

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function showNextDiv() {
     // Get all div elements with the class
     const divs = document.querySelectorAll('.message-container');
     const container = document.getElementById('container');
+    const sound = document.getElementById('notification-sound')
+    
 
     // Check if there are more divs to show
     if (currentDiv < divs.length) {
@@ -13,6 +15,8 @@ function showNextDiv() {
 
         // Automatically scroll to the bottom of the container
         container.scrollTop = container.scrollHeight;
+
+        sound.play();
     }
     
 }
