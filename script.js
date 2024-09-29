b729839fd32782d1808a102d322b744d21627193
Wenga <mankayanwenga@gmail.com>
2024-09-28 21:52:58 -0700

    scrollbar to the end

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ let currentDiv = 0;
 function showNextDiv() {
     // Get all div elements with the class
     const divs = document.querySelectorAll('.message-container');
+    const container = document.getElementById('container');
 
     // Check if there are more divs to show
     if (currentDiv < divs.length) {
-        divs[currentDiv].classList.remove('hidden');  // Remove 'hidden' class
-        divs[currentDiv].classList.add('visible');    // Add 'visible' class
+        divs[currentDiv].classList.remove('hidden');  
+        divs[currentDiv].classList.add('visible');    
         currentDiv++;  // Move to the next div
+
+        // Automatically scroll to the bottom of the container
+        container.scrollTop = container.scrollHeight;
     }
+    
 }
